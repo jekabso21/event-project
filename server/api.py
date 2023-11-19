c9392acf06806cdd007903c163cb43075034ff9e
Retro21 <56453764+jekabso21@users.noreply.github.com>
2023-11-19 02:08:43 +0200

    ss

--- server/api.py
+++ server/api.py
@@ -3,17 +3,19 @@ import sys
 import mariadb
 from fastapi import FastAPI
 from pydantic import BaseModel
+import hashlib
 
 # Connect to MariaDB Platform
 try:
     conn = mariadb.connect(
         user="root",
         password="GKCi651wSa3LutHX",
-        host="15.204.213.220",
+        host="127.0.0.1",
         port=3306,
         database="party-event"
 
     )
+    print("Connected to MariaDB Platform")
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: {e}")
     sys.exit(1)
@@ -21,12 +23,11 @@ except mariadb.Error as e:
 # Get Cursor
 cur = conn.cursor()
 
-print(cur.execute("SELECT * FROM admin"))
 
-
-class UserPost(BaseModel):
+class UserData(BaseModel):
     name: str
-    body: str
+    email: str
+    group: str
 
 
 app = FastAPI()
@@ -37,25 +38,32 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/join/adduser")
-async def add_user():
-    return {"message": "User added"}
-
-
-@app.post("/join/adduser")
-async def add_user(req: dict):
-    return {"message": "User added", "DATA": "data Hello World"}
-
-
 posts = []
 
+@app.post("/join/adduser")
+async def add_user(data: UserData):
+    qr_data = hashlib.md5(data.name.encode('UTF-8'))
+    qr_data = qr_data.hexdigest()
+    try:
+        cur.execute("INSERT INTO users (name, email, `group`, qrcode) VALUES (%s, %s, %s, %s)", (data.name, data.email, data.group, 'lel'))
+        conn.commit()
+    except mariadb.Error as e:
+        print(f"Error: {e}")
 
-@app.post("/post", response_model=UserPost)
-async def create_post(post: UserPost):
-    posts.append(post)
-    return post
+    return data
 
 
-@app.get("/posts", response_model=list[UserPost])
-async def get_all_posts():
-    return posts
+# @app.post("/post", response_model=UserPost)
+# async def create_post(post: UserPost):
+#     posts.append(post)
+#     return post
+#
+#
+# @app.get("/posts", response_model=list[UserPost])
+# async def get_all_posts():
+#     return posts
+
+@app.delete("/posts/{post_id}")
+async def delete_post(post_id: int):
+    posts.pop(post_id - 1)
+    return {}
